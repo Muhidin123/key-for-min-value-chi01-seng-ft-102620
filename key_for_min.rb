bc4e70abcce1f88d736d753022154464cc209227
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-18 18:23:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,4 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+\def key_for_min_value(name_hash)
+  new_arr = name_hash.to_a
+  value_to_compare = 100000000
+  key_to_store = {}
+  new_arr.each do |value|
+    if value[1] <= value_to_compare
+      value_to_compare = value[1]
+      key_to_store = value[0]
+    end
+    return key_to_store
+  end
+end
