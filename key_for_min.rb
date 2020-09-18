ddb3fa48435957dea2283e9b46a9970f1114749b
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-18 18:33:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   value_to_compare = 100000000
-  key_to_store = {}
+  key_to_store = nil
   name_hash.each do |key, value|
     if value <= value_to_compare
       value_to_compare = value
