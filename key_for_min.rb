f61c48335059ed2057c91be3ab311caab88b07c3
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-18 18:19:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,3 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  value_to_compare = 1000000000000
-  solution = {}
-  name_hash.each do |key , value|
-    if value < value_to_compare
-      solution[key] = value
-      puts solution
-    end
-  end
-end
